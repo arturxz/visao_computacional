38074b67d6f04fbd66f8806deab32e2a40eee88a
Artur Santos Nascimento <arturxz@outlook.com.br>
2020-04-28 23:08:02 -0300

    fft e ifft aparentemente corretas

--- lab/lab_fft/lab_fft.py
+++ lab/lab_fft/lab_fft.py
@@ -46,7 +46,7 @@ def nchannels( img ):
 		else:
 			return img.shape[2]
 	else:
-		return -1
+		return None
 
 def imshow( img ):
 	plt = mp.imshow(img, cmap=mp.gray(), origin="upper", vmin=0, vmax=255)
@@ -77,4 +77,110 @@ def rgb2gray( img ):
 			gray = 0.299 * r + 0.587 * g + 0.114 * b
 			return np.asarray(gray, dtype=np.uint8)
 	return None
+
+def dft( arr ):
+	if( isinstance( arr, np.ndarray ) ):
+		# GARANTE QUE O ARRAY É FLOAT
+		x = np.asarray( arr, np.complex )
+		
+		# PREPARA OS VALORES DA SOMATORIA
+		N = arr.shape[0]
+		n = np.arange( N )
+		k = n.reshape( ( N, 1) )
+		M = np.exp( -2j * np.pi * k * n / N )
+		
+		# RETORNA A MULTIPLICACAO FINAL
+		return np.dot( M, x )
+
+
+def fft( arr ):
+	if( isinstance( arr, np.ndarray ) ):
+		x = np.asarray( arr, np.complex )
+		N = x.shape[0]
+
+		if( N % 2 != 0 ):
+			raise ValueError( "Precisa ser potência de 2" )
+		elif( N <= 2 ):
+			return dft( arr ) # CASO BASE DA RECURSAO
+		else:
+			X_pares = fft( x[ ::2 ] ) # COMPUTA OS PARES
+			X_impar = fft( x[ 1::2 ] ) # COMPUTA OS IMPARES
+			terms = np.exp( -2j * np.pi * np.arange( N ) / N ) # PREPARA O EXPONENCIAL
+
+			fu = X_pares + terms[ :int( N/2 ) ] * X_impar # CALCULA f(u)
+			fu_k = X_pares + terms[ int( N/2 ): ] * X_impar # CALCULA f(u) + k
+
+			return np.concatenate( [ fu, fu_k ] )
+
+def fft_2d( arr ):
+	print( "fft_2d" )
+	if( isinstance( arr, np.ndarray ) ):
+		arr_fourrier = np.asarray( arr, np.complex )
+		if( nchannels( arr ) == 1 ):
+			for i in range( arr.shape[ 0 ] ):
+				arr_fourrier[i, :] = fft( arr[i, :] ) # FAZENDO PARA EIXO X
+			
+			for j in range( arr.shape[ 1 ] ):
+				arr_fourrier[:, j] = fft( arr_fourrier[:, j] ) # FAZENDO PARA EIXO Y
+		elif( nchannels( arr ) == 3 ):
+			for canal in range( 3 ):
+				arr_fourrier[:, :, canal] = fft_2d( arr[:, :, canal] ) # FAZENDO PARA CADA CANAL DE COR
+		
+		return arr_fourrier
+
+def ifft( arr_fourrier ):
+	if( isinstance( arr_fourrier, np.ndarray ) ):
+		# GARANTINDO QUE O ARRAY EH DE COMPLEXOS
+		arr_fourrier = np.asarray( arr_fourrier, np.complex )
+
+		# RETORNA A CONJUGAÇÃO DA PARTE IMAGINARIA
+		arr_fourrier_conjugate = np.conjugate( arr_fourrier )
+		
+		# CALCULA O FOURRIER DO ARRAY CONJUGADO
+		arr = fft( arr_fourrier_conjugate )
+		
+		# RECONJUGANDO
+		arr = np.conjugate( arr )
+
+		# DIVISAO POR M
+		arr = arr / arr_fourrier.shape[0]
+		return arr
+
+def ifft_2d( arr_fourrier ):
+	print( "ifft_2d" )
+	if( isinstance( arr_fourrier, np.ndarray ) ):
+		arr = np.asarray( arr_fourrier, np.complex )
+		if( nchannels( arr_fourrier ) == 1 ):
+			for i in range( arr_fourrier.shape[0] ):
+				arr[i, :] = ifft( arr_fourrier[i, :] ) # FAZENDO PARA EIXO X
+			
+			for j in range( arr_fourrier.shape[1] ):
+				arr[:, j] = ifft( arr[:, j] ) # FAZENDO PARA EIXO Y
+		elif( nchannels( arr_fourrier ) == 3 ):
+			for canal in range( 3 ):
+				arr[:, :, canal] = ifft_2d( arr[:, :, canal] ) # FAZENDO PARA CADA CANAL DE COR
+		
+		return arr.astype( np.float )
+		
+
+""" 
+	Main
+"""
+listaImagens = retornaListaArquivos()
+path = listaImagens[0]
+
+img1 = imread( path )
+img2 = fft_2d( img1 )
+
+ifft_img1 = ifft_2d( img1 )
+ifft_img2 = ifft_2d( img2 )
+
+img1 = img1.astype( np.uint8 )
+img2 = img2.astype( np.uint8 )
+
+ifft_img1 = ifft_img1.astype( np.uint8 )
+ifft_img2 = ifft_img2.astype( np.uint8 )
+
+compare_images( img1, img2, "Original", "Fourrier" )
+compare_images( ifft_img1, ifft_img2, "Fourrier Inversa de Original", "Fourrier Inversa de Fourrier" )
 
